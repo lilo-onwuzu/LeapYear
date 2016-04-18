9eec82f51b5e69d45469a32b9ac141e3554767ea
Lilo Onwuzu <lpr422@gmail.com>
2016-04-18 15:47:50 -0700

    Leap Year Code works

---
+++ src/test/java/LeapYearTest.java
@@ -0,0 +1,27 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class LeapYearTest{
+
+  @Test
+  public void isLeapYear_forNumberDivisibleByFour_true(){
+    LeapYear testLeapYear = new LeapYear();
+    assertEquals(true, testLeapYear.isLeapYear(2012));
+  }
+  @Test
+  public void isLeapYear_forNumberNotDivisibleByFour_false(){
+    LeapYear testLeapYear = new LeapYear();
+    assertEquals(false, testLeapYear.isLeapYear(2011));
+  }
+
+  @Test
+  public void isLeapYear_forMultiplesOfOneHundred_false() {
+    LeapYear testLeapYear = new LeapYear();
+    assertEquals(false, testLeapYear.isLeapYear(1900));
+  }
+  @Test
+  public void isLeapYear_forMultipleOfFourHundred_true(){
+    LeapYear testLeapYear = new LeapYear();
+    assertEquals(true, testLeapYear.isLeapYear(2000));
+  }
+}
